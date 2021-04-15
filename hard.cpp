5ef1e30e2b2952bd68e74902060ec6e3864747de
Suraj Choubey <srjchoubey2@gmail.com>
2021-04-15 12:11:07 +0530

    major changes 2

--- hard.cpp
+++ hard.cpp
@@ -3,47 +3,52 @@
 #include <bits/stdc++.h>
 
 #define pi 3.141592
-#define epsilon 1e-6
-// can also use M_PI
 
 using namespace std;
 
-double proportional_probability_density_gaussian(double x, double mu, double sigma)
+// Normal function
+double density_function(double, double, double);
+// Metropolis Hastings function
+void metro_hastings(double, double, int, int ,int, int);
+
+int main()
 {
-    //The Proportional Distribution used is the exponential part of the Univariate Truncated Gaussian
-    double ans = (double)exp(-pow((x - mu) / sigma, 2) / 2);
-    return ans;
+    srand(time(0));
+    metro_hastings(0, 1, -3, 3);
+    return 0;
 }
 
-void metropolis_hastings(double mu, double sigma, int x_min, int x_max, int y_min = 0, int y_max = 0.5)
+double density_function(double x, double mu, double sigma)
 {
-    // Writing sampled values to external file
-    ofstream outfile;
-    outfile.open("data1.txt");
+    double y = (double)exp(-pow((x - mu) / sigma, 2) / 2);
+    return y;
+}
+
+void metro_hastings(double mu, double sigma, int x_min, int x_max, int y_min = 0, int y_max = 0.5)
+{
+    ofstream myfile;
+    myfile.open("hard_output.csv");
 
     // sample generator from the in-built normal distribution
     default_random_engine e(0);
-
-    // Initialising a random state for x between x_min and x_max
     double rand_x = (double)x_min + (rand() % ((x_max - x_min + 1) * 100) / (double)100);
 
-    // Total accepted transitions, ideally this value should correspond to 50% for the case of univariate gaussian
+    // Total accepted transitions
     int jumps = 0;
 
     // sampling for 10000 samples
-    for (long long i = 0; i < 10000; ++i)
+    for (long i = 0; i < 10000; ++i)
     {
         // transition distribution chosen is a univariate gaussian centred at previous sample
         normal_distribution<double> jumping_distribution(rand_x, 2);
 
-        // sample a new value
         double rand_x_new = x_max + 1;
         while (rand_x_new < x_min || rand_x_new > x_max)
         {
             rand_x_new = jumping_distribution(e);
         }
 
-        double alpha = proportional_probability_density_gaussian(rand_x_new, mu, sigma) / proportional_probability_density_gaussian(rand_x, mu, sigma);
+        double alpha = density_function(rand_x_new, mu, sigma) / density_function(rand_x, mu, sigma);
 
         // sample from U(0, 1)
         double u = (rand() % 101) / (double)100;
@@ -55,17 +60,10 @@ void metropolis_hastings(double mu, double sigma, int x_min, int x_max, int y_mi
             jumps += 1;
         }
 
-        outfile << rand_x << endl;
+        myfile << rand_x << endl;
     }
 
     cout << (double)jumps * 100 / 10000;
-    outfile.close();
+    myfile.close();
     return;
-}
-
-int main()
-{
-    srand(time(0));
-    metropolis_hastings(0, 1, -5, 5);
-    return 0;
 }
--- medium.cpp
+++ medium.cpp
@@ -3,48 +3,40 @@
 #include <bits/stdc++.h>
 
 #define pi 3.141592
-#define epsilon 1e-6
-// can also use M_PI
 
 using namespace std;
 
-double probability_density_gaussian(double x, double mu, double sigma)
+double density_function(double x, double mu, double sigma)
 {
-    // return the probability density of a sample from univariate gaussian
-    double ans = (double)exp(-pow((x - mu) / sigma, 2) / 2) / (sqrt(2 * pi));
-    return ans;
+    double y = (double)exp(-pow((x - mu) / sigma, 2) / 2) / (sqrt(2 * pi));
+    return y;
 }
 
-void rejection_sampling(double mu, double sigma, int x_min, int x_max, int y_min = 0, int y_max = 0.5)
+void rejection_sampling(double mu, double sigma, int x_min, int x_max, int y_min = 0, int y_max = 1)
 {
-    // Store samples in output file
-    ofstream outfile;
-    outfile.open("data3.txt");
+    // Storing samples in medium_output.csv
+    ofstream myfile;
+    myfile.open("medium_output.csv");
 
     // erf is the error function, used to calculate the normalising factor for truncated gaussian
     double erf_x_max = erf((x_max - mu) / sigma);
     double erf_x_min = erf((x_min - mu) / sigma);
-    cout << erf_x_max << " " << erf_x_min << endl;
 
-    for (long long i = 0; i < 100000; ++i)
+    for (long i = 0; i < 10; ++i)
     {
-        // uniformly sample a x-value
         double rand_x = (double)x_min + (rand() % ((x_max - x_min + 1) * 100) / (double)100);
 
-        // uniformly sample corresponding probability density
         double rand_y = (double)y_min + (rand() % ((y_max - y_min + 1) * 100) / (double)100);
 
-        // actual probability density
-        double cdf_x = probability_density_gaussian(rand_x, mu, sigma) / (sigma * (erf_x_max - erf_x_min));
+        double cdf_x = density_function(rand_x, mu, sigma) / (sigma * (erf_x_max - erf_x_min));
 
         if (rand_y <= cdf_x)
         {
-            // acceptable sample
-            outfile << rand_x << "," << rand_y << endl;
+            myfile << rand_x << "," << rand_y << endl;
         }
     }
 
-    outfile.close();
+    myfile.close();
     return;
 }
 
